e989d11c8f80e1dd78c00bad0ef49ffa6fe98bb7
Nurlan <nurlanea@code.edu.az>
2024-09-17 22:41:53 +0400

    Login system

--- source/FinalProject.API/Controllers/JobPostController.cs
+++ source/FinalProject.API/Controllers/JobPostController.cs
@@ -1,18 +1,27 @@
 ﻿using FinalProject.Domain.Reporistories;
 using Microsoft.AspNetCore.Mvc;
+using User.Module.Commands;
+using User.Module.Services;
 
 namespace FinalProject.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class JobPostController(IJobRepository jobRepo) : ControllerBase
+    public class JobPostController(IJobRepository jobRepo, ILoginService loginService) : ControllerBase
     {
         private readonly IJobRepository _jobRepository = jobRepo;
+        private readonly ILoginService _loginService = loginService;
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _jobRepository.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            return Ok(await _loginService.Login(command.Email, command.Password));
         }
     }
 }
--- source/FinalProject.API/Program.cs
+++ source/FinalProject.API/Program.cs
@@ -1,7 +1,14 @@
 using FinalProject.Application.Usecase;
 using FinalProject.Domain.Reporistories;
 using FinalProject.Infrastructure.DAL;
+using FinalProject.SharedKernel.Domain.Settings;
+using Identity.Module.Auth;
+using IdentityModule.Auth;
+using IdentityModule.Queries;
+using Infrastructure.Identity;
 using Infrastructure.Middleware;
+using System.Reflection;
+using User.Module.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,8 +22,27 @@ builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddLogging();
 
 #region Service Registry
+Assembly.Load("Identity.Module");
+Assembly.Load("User.Module");
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddScoped<IUserManager, UserManager>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserQueries, UserQueries>();
+builder.Services.AddScoped<IClaimsManager, ClaimsManager>();
+builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddTransient<GlobalExceptionHandler>();
+
+foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+{
+    builder.Services.AddAutoMapper(assembly);
+}
+#endregion
+
+#region Configurations
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.Configure<JWTSettings>(configuration.GetSection(nameof(JWTSettings)));
 #endregion
 
 var app = builder.Build();

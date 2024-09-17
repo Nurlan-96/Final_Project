28bb7dc68d1307c58a6682c99527aa72b6e6af41
Nurlan <nurlanea@code.edu.az>
2024-09-17 22:41:02 +0400

    Typo

--- source/FinalProject.Infrastructure/Configurations/CategoryEntityConfiguration.cs
+++ source/FinalProject.Infrastructure/Configurations/CategoryEntityConfiguration.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace FinalProject.Infrastructure.Configurations
 {
-    public class CategoriesEntityConfiguration : IEntityTypeConfiguration<Category>
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
     {
         public void Configure(EntityTypeBuilder<Category> builder)
         {
             builder.ToTable("category");
             builder.HasKey(c => c.Id);
 
-            builder.Property(c => c.Name).HasMaxLength(60).HasColumnName("name");
+            builder.Property(c => c.Name).HasMaxLength(50).HasColumnName("name");
             builder.HasIndex(c => c.Name).IsUnique();
         }
     }

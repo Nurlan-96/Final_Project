87b3e46533d163682329f968b647572ee2abdab5
Nurlan <nurlanea@code.edu.az>
2024-09-21 19:52:55 +0400

    added HttpContextAccessor, Scopes, Serilog, StaticFiles

--- source/FinalProject.API/Program.cs
+++ source/FinalProject.API/Program.cs
@@ -8,8 +8,10 @@ using IdentityModule.Queries;
 using Infrastructure.Identity;
 using Infrastructure.Middleware;
 using System.Reflection;
+using FluentValidation;
 using User.Module.Services;
-using UserModule.Handlers;
+using Job.Module.Service;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,24 +23,38 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddLogging();
+builder.Services.AddHttpContextAccessor();
 
 #region Service Registry
 Assembly.Load("Identity.Module");
 Assembly.Load("User.Module");
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.AddScoped<IJobService, JobService>();
+builder.Services.AddScoped<ICompanyService, CompanyService>();
+builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+#region User/Authorization
 builder.Services.AddScoped<IUserManager, UserManager>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserQueries, UserQueries>();
 builder.Services.AddScoped<IClaimsManager, ClaimsManager>();
-builder.Services.AddScoped<ILoginService, LoginService>();
-builder.Services.AddScoped<IRegisterService, RegisterService>();
+builder.Services.AddScoped<IAuthorizationService, AuthorizationService>();
+#endregion
 builder.Services.AddTransient<GlobalExceptionHandler>();
 
-foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
 {
     builder.Services.AddAutoMapper(assembly);
+    builder.Services.AddValidatorsFromAssembly(assembly);
 }
+#endregion
+
+#region SeriLog
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.Console()
+    .WriteTo.File("logs/Log-.txt",rollingInterval: RollingInterval.Day).CreateLogger();
+builder.Host.UseSerilog();
 #endregion
 
 #region Configurations
@@ -48,7 +64,7 @@ builder.Services.Configure<JWTSettings>(configuration.GetSection(nameof(JWTSetti
 #endregion
 
 var app = builder.Build();
-
+app.UseStaticFiles();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -56,9 +72,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();

1743f6ebbabca9265d3d1e8b36f53275dbfaccce
Nurlan <nurlanea@code.edu.az>
2024-09-21 19:38:28 +0400

    added an ImageUploadExtension

---
+++ source/FinalProject.SharedKernel.Domain/Extensions/ImageUploadExtension.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace FinalProject.SharedKernel.Domain.Extensions
+{
+    public static class ImageUploadExtension
+    {
+        public static async Task<string> UploadImageAsync(this IFormFile file, string uploadPath, long maxFileSizeInBytes, IHttpContextAccessor httpContextAccessor)
+        {
+            if (file == null || file.Length == 0)
+            {
+                throw new ArgumentException("File is empty.");
+            }
+
+            if (file.Length > maxFileSizeInBytes)
+            {
+                throw new ArgumentException($"File size exceeds the limit of {maxFileSizeInBytes / (1024 * 1024)} MB.");
+            }
+
+            if (!file.ContentType.StartsWith("image"))
+            {
+                throw new ArgumentException("Invalid file type. Only image files are allowed.");
+            }
+
+            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+            var filePath = Path.Combine(uploadPath, fileName);
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            var request = httpContextAccessor.HttpContext.Request;
+            var baseUrl = $"{request.Scheme}://{request.Host.Value}";
+
+            string relativePath = Path.Combine("wwwroot/img", Path.GetFileName(uploadPath), fileName);
+            return $"{baseUrl}/{relativePath.Replace("\\", "/")}";
+        }
+    }
+
+
+}

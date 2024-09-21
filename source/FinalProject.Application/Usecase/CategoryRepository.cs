527f32a7fe8c5bb15e70b837d6477fb74fc01707
Nurlan <nurlanea@code.edu.az>
2024-09-21 19:43:28 +0400

    added repositories for category and company

---
+++ source/FinalProject.Application/Usecase/CategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Usecases;
+using FinalProject.Domain.Entities;
+using FinalProject.Domain.Reporistories;
+using FinalProject.Infrastructure.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Application.Usecase
+{
+    public class CategoryRepository(AppDbContext context) : Repository<Category>, ICategoryRepository
+    {
+        public sealed override DbContext Context { get; protected set; } = context;
+    }
+}
---
+++ source/FinalProject.Application/Usecase/CompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Usecases;
+using FinalProject.Domain.Entities;
+using FinalProject.Domain.Reporistories;
+using FinalProject.Infrastructure.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Application.Usecase
+{
+    public class CompanyRepository(AppDbContext context) : Repository<Company>, ICompanyRepository
+    {
+        public sealed override DbContext Context { get; protected set; } = context;
+    }
+}

4388c3eb9129ef2d1842ff59ad3f74671bd67558
Nurlan <nurlanea@code.edu.az>
2024-09-21 19:47:44 +0400

    injected IJobservice into JobPostController

--- source/FinalProject.API/Controllers/JobPostController.cs
+++ source/FinalProject.API/Controllers/JobPostController.cs
@@ -1,34 +1,38 @@
 ﻿using FinalProject.Domain.Reporistories;
+using Job.Module.Command;
+using Job.Module.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using User.Module.Commands;
-using User.Module.Services;
 
 namespace FinalProject.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class JobPostController(IJobRepository jobRepo, ILoginService loginService, IRegisterService registerService) : ControllerBase
+    public class JobPostController(IJobRepository jobRepo, IJobService jobService): ControllerBase
     {
         private readonly IJobRepository _jobRepository = jobRepo;
-        private readonly ILoginService _loginService = loginService;
-        private readonly IRegisterService _registerService = registerService;
+        private readonly IJobService _jobService = jobService;
 
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             return Ok(await _jobRepository.GetAllAsync());
         }
-
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        [HttpPost("Create")]
+        public async Task<IActionResult> PostJob([FromBody] CreateJobCommand command)
         {
-            return Ok(await _loginService.Login(command));
+            return Ok(await _jobService.CreateJobPost(command));
         }
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterCommand command, CancellationToken cancellationToken)
+        [HttpPut]
+        public async Task<IActionResult> UpdateJob([FromBody] UpdateJobCommand command)
         {
-            return Ok(await _registerService.Register(command, cancellationToken));
+            return Ok(await _jobService.UpdateJobPost(command));
+        }
+        [HttpPatch]
+        public async Task<IActionResult> ArchiveJob([FromBody] UpdateJobCommand command)
+        {
+            return Ok(await _jobService.ArchiveJobPost(command));
         }
     }
 }

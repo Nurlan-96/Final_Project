b32a85d6dd623beab78498021b4edaa6493bb2fd
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:57:38 +0400

    added Authorize Roles Attribute

---
+++ source/FinalProject.Domain/Attributes/AuthroizeRolesAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Domain.Attributes
+{
+    public class AuthroizeRolesAttribute : AuthorizeAttribute
+    {
+        public AuthroizeRolesAttribute(params string[] roles) : base()
+        {
+            Roles = string.Join(",", roles);
+        }
+    }
+}

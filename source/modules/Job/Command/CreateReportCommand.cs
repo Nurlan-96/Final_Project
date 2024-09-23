146d5ea7de4b02a57b4db0aad434d1623d3d0664
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:54:45 +0400

    added Report Commands

---
+++ source/modules/Job/Command/CreateReportCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Module.Command
+{
+    public class CreateReportCommand
+    {
+        public string Reason { get; set; }
+        public int JobPostId { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ source/modules/Job/Command/UpdateReportCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job.Module.Command
+{
+    public class UpdateReportCommand
+    {
+        public int ReportId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

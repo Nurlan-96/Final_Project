683057b45aa96521a03ff178b2f571989fe02772
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:50:19 +0400

    added AuthenticationController to admin

---
+++ source/FinalProject.Admin.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,26 @@
+﻿using FinalProject.Domain.Reporistories;
+using Job.Module.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using User.Module.Commands;
+
+namespace FinalProject.Admin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController(User.Module.Services.IAuthorizationService loginService) : ControllerBase
+    {
+        private readonly User.Module.Services.IAuthorizationService _authorizationService = loginService;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterCommand command, CancellationToken cancellationToken)
+        {
+            return Ok(await _authorizationService.Register(command, cancellationToken));
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            return Ok(await _authorizationService.Login(command));
+        }
+    }
+}

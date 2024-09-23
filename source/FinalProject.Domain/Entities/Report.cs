5cebd8ca4aa469d3773a4ade3893a9ec2e9cb38a
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:49:34 +0400

    added a Report Entity

---
+++ source/FinalProject.Domain/Entities/Report.cs
@@ -0,0 +1,12 @@
+﻿using FinalProject.SharedKernel.Domain.Seedwork;
+
+namespace FinalProject.Domain.Entities
+{
+    public class Report : BaseEntity
+    {
+        public string Reason { get; set; }
+        public int JobPostId { get; set; }
+        public int UserId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}

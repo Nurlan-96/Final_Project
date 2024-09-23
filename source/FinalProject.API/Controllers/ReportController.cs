737daf6ab7e24560879f06b7578cf4cf25b2cd90
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:54:12 +0400

    added Report controller

---
+++ source/FinalProject.API/Controllers/ReportController.cs
@@ -0,0 +1,22 @@
+﻿using FinalProject.Domain.Reporistories;
+using Job.Module.Command;
+using Job.Module.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController(IReportRepository reportRepo, IReportService reportService) : ControllerBase
+    {
+        private readonly IReportRepository _reportRepository = reportRepo;
+        private readonly IReportService _reportService = reportService;
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateCompany([FromForm] CreateReportCommand command)
+        {
+            return Ok(await _reportService.CreateReport(command));
+        }
+    }
+}

cdd07e5577fa56abf309df1e2e6379dc9574328b
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:54:32 +0400

    added Category Queries

---
+++ source/modules/Job/Queries/CategoryQuery.cs
@@ -0,0 +1,24 @@
+﻿using Application.Response;
+using Domain.Exceptions;
+using FinalProject.Domain.Entities;
+using FinalProject.Domain.Reporistories;
+
+namespace Job.Module.Queries
+{
+    public class CategoryQuery(ICategoryRepository catRepo) : ICategoryQuery
+    {
+        private readonly ICategoryRepository _catRepo = catRepo;
+        public async Task<Pagination<Category>> GetAllCategories(int page, int size)
+        {
+            var data = await _catRepo.GetAllAsync();
+            var paginated = new Pagination<Category>(data, page, size);
+            return paginated;
+        }
+
+        public async Task<Category> GetCategoryById(int id)
+        {
+            return await _catRepo.GetWhere(x => x.Id == id) 
+                ?? throw new EntityNotFoundException<Category>();
+        }
+    }
+}
---
+++ source/modules/Job/Queries/ICategoryQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Response;
+using FinalProject.Domain.Entities;
+
+namespace Job.Module.Queries
+{
+    public interface ICategoryQuery
+    {
+        Task<Pagination<Category>> GetAllCategories(int page, int size);
+        Task<Category> GetCategoryById(int id);
+    }
+}

f7f9afcc711c9cb5f7284b3cffe3c6ffaca5b48e
Nurlan <nurlanea@code.edu.az>
2024-09-23 22:50:04 +0400

    added CategoryController to admin

---
+++ source/FinalProject.Admin.API/Controllers/CategoryController/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Attributes;
+using Domain.Entities.RoleAggergate;
+using Job.Module.Command;
+using Job.Module.Queries;
+using Job.Module.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Admin.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class CategoryController(ICategoryService categoryService, ICategoryQuery categoryQuery) : ControllerBase
+    {
+        private readonly ICategoryService _categoryService = categoryService;
+        private readonly ICategoryQuery _categoryQuery = categoryQuery;
+
+        [HttpPost("Create")]
+        [AuthroizeRolesAttribute(Roles = RoleName.SuperAdmin)]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryCommand command)
+        {
+            return Ok(await _categoryService.CreateCategory(command));
+        }
+
+        [HttpPatch]
+        [AuthroizeRolesAttribute(Roles = RoleName.SuperAdmin)]
+        public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryCommand command)
+        {
+            return Ok(await _categoryService.UpdateCategory(command));
+        }
+
+        [HttpDelete]
+        [AuthroizeRolesAttribute(Roles = RoleName.SuperAdmin)]
+        public async Task<IActionResult> DeleteCategory([FromBody] int categoryId)
+        {
+            return Ok(await _categoryService.DeleteCategory(categoryId));
+        }
+
+    }
+}
---
+++ source/FinalProject.Admin.API/Controllers/CategoryController/CategoryQueryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Admin.API.Controllers
+{
+    public partial class CategoryController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] int page, int size)
+        {
+            return Ok(await _categoryQuery.GetAllCategories(page, size));
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> GetById([FromForm] int id)
+        {
+            return Ok(await _categoryQuery.GetCategoryById(id));
+        }
+    }
+}

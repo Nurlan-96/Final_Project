ba7eeb531465c6d728e2442b32680c3d3722eb28
Nurlan <nurlanea@code.edu.az>
2024-09-24 22:51:09 +0400

    updated badatabse to include Reports

---
+++ source/FinalProject.Infrastructure.Migrations/Migrations/20240923190028_addedReport.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FinalProject.Infrastructure.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedReport : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Reason = table.Column<string>(type: "text", nullable: false),
+                    JobPostId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 23, 19, 0, 27, 810, DateTimeKind.Utc).AddTicks(1333));
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 23, 19, 0, 27, 810, DateTimeKind.Utc).AddTicks(1339));
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 23, 19, 0, 27, 810, DateTimeKind.Utc).AddTicks(1340));
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "password" },
+                values: new object[] { new DateTime(2024, 9, 23, 19, 0, 27, 810, DateTimeKind.Utc).AddTicks(1477), "AQAAAAEACSfAAAAAEBbWs4xACSTqqRgmrn8dyEHw3SfnkADU0QQmwyb+R3xxgATDB2Rk0lHb76FJmkKctA==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 20, 16, 54, 6, 29, DateTimeKind.Utc).AddTicks(8720));
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 20, 16, 54, 6, 29, DateTimeKind.Utc).AddTicks(8731));
+
+            migrationBuilder.UpdateData(
+                table: "role",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 20, 16, 54, 6, 29, DateTimeKind.Utc).AddTicks(8733));
+
+            migrationBuilder.UpdateData(
+                table: "user",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "password" },
+                values: new object[] { new DateTime(2024, 9, 20, 16, 54, 6, 29, DateTimeKind.Utc).AddTicks(8857), "AQAAAAEACSfAAAAAEIMhGWKCabZvqbGnFJD7I09fzKdEq0/11LsdbQqxCVXoIG1rC55GFgk7rKwCau5gYQ==" });
+        }
+    }
+}

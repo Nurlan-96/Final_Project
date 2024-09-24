17770ce86f56f790de874ad3f2b700b9de304abe
Nurlan <nurlanea@code.edu.az>
2024-09-24 22:49:14 +0400

    created job queries

---
+++ source/modules/Job/Queries/IJobQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Response;
+using FinalProject.Domain.Entities;
+
+namespace Job.Module
+{
+    public interface IJobQuery
+    {
+        Task<Pagination<JobPost>> GetAllJobs(int page, int size);
+        Task<JobPost> GetJobById(int id);
+    }
+}
---
+++ source/modules/Job/Queries/JobQuery.cs
@@ -0,0 +1,24 @@
+﻿using Application.Response;
+using Domain.Exceptions;
+using FinalProject.Domain.Entities;
+using FinalProject.Domain.Reporistories;
+
+namespace Job.Module.Queries
+{
+    public class JobQuery(IJobRepository jobRepo) : IJobQuery
+    {
+        private readonly IJobRepository _jobRepo = jobRepo;
+        public async Task<Pagination<JobPost>> GetAllJobs(int page, int size)
+        {
+            var data = await _jobRepo.GetAllAsync();
+            var paginated = new Pagination<JobPost>(data, page, size);
+            return paginated;
+        }
+       
+        public async Task<JobPost> GetJobById(int id)
+        {
+            return await _jobRepo.GetWhere(x => x.Id == id)
+                ?? throw new EntityNotFoundException<JobPost>();
+        }
+    }
+}

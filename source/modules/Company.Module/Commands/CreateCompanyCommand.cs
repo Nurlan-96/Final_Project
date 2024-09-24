759059b44af55302c8109162c583b5c5cd5c426a
Nurlan <nurlanea@code.edu.az>
2024-09-24 22:48:56 +0400

    created company module

---
+++ source/modules/Company.Module/Commands/CreateCompanyCommand.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Company.Module.Commands
+{
+    public class CreateCompanyCommand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ source/modules/Company.Module/Services/ICompanyService.cs
@@ -0,0 +1,14 @@
+﻿using Company.Module.Commands;
+using Microsoft.AspNetCore.Http;
+
+namespace Company.Module.Services
+{
+    public interface ICompanyService
+    {
+        public Task<bool> CreateCompany(CreateCompanyCommand command);
+        public Task<bool> UpdateCompany(UpdateCompanyCommand command);
+        public Task<bool> DeleteCompany(int companyId);
+        public Task<bool> ArchiveCompany(UpdateCompanyCommand command);
+
+    }
+}

5d3724b7e8f1afe9d9c847b7de90cf50c48e7039
Nurlan <nurlanea@code.edu.az>
2024-10-06 15:32:17 +0400

    created responses for category and job posts

---
+++ source/modules/Job/Responses/CategoryResponse.cs
@@ -0,0 +1,12 @@
+﻿using Company.Module.Responses;
+using FinalProject.Domain.Constants;
+using FinalProject.Domain.Entities;
+
+namespace Job.Module.Responses
+{
+    public class CategoryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ source/modules/Job/Responses/JobPostResponse.cs
@@ -0,0 +1,21 @@
+﻿using Company.Module.Responses;
+using FinalProject.Domain.Constants;
+
+namespace Job.Module.Responses
+{
+    public class JobPostResponse
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; }
+        public string Address { get; set; }
+        public int Salary { get; set; }
+        public CompanyResponse Company { get; set; }
+        public CategoryResponse Category { get; set; }
+        public CityEnum City { get; set; }
+        public EducationEnum Education { get; set; }
+        public EmploymentTypeEnum EmploymentType { get; set; }
+        public ExperienceEnum Experience { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}

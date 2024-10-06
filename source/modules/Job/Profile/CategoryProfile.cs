4d8db2749d5c8771bda52c4c956054bb71977f31
Nurlan <nurlanea@code.edu.az>
2024-10-06 15:32:41 +0400

    created category and jobprofile

---
+++ source/modules/Job/Profile/CategoryProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FinalProject.Domain.Entities;
+using Job.Module.Responses;
+
+namespace Job.Module.Profiles
+{
+    public class CategoryProfile : Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<JobPostEntity, JobPostResponse>();
+            CreateMap<CategoryEntity, CategoryResponse>();
+        }
+    }
+}
---
+++ source/modules/Job/Profile/JobProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Company.Module.Responses;
+using FinalProject.Domain.Entities;
+using Job.Module.Responses;
+
+namespace Job.Module.Profiles
+{
+    public class JobProfile : Profile
+    {
+        public JobProfile()
+        {
+            CreateMap<JobPostEntity, JobPostResponse>();
+            CreateMap<CompanyEntity, CompanyResponse>();
+        }
+    }
+}
